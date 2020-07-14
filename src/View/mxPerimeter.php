b405a85d6091e9f6b79969ded038a09f9652a3a7
Emiel van Goor <emielvangoor@me.com>
2020-07-14 16:56:58 +0200

    Created composer package of the mxGraph library.
    Added PSR-4 autoloading and namespaces.

---
+++ src/View/mxPerimeter.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * EXB R5 - Business suite
+ * Copyright (C) EXB Software 2020 - All Rights Reserved
+ *
+ * This file is part of EXB R5.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @author emiel <e.goor@exb-software.com>
+ */
+declare(strict_types=1);
+
+namespace Mxgraph\View;
+
+
+class mxPerimeter
+{
+    /**
+     * Variable: RectanglePerimeter
+     *
+     * Provides a rectangular perimeter.
+     */
+    public static $RectanglePerimeter;
+
+    /**
+     * Variable: EllipsePerimeter
+     *
+     * Provides an elliptic perimeter.
+     */
+    public static $EllipsePerimeter;
+
+    /**
+     * Variable: RhombusPerimeter
+     *
+     * Provides a rhombus (aka diamond) perimeter.
+     */
+    public static $RhombusPerimeter;
+
+    /**
+     * Variable: TrianglePerimeter
+     *
+     * Provides a triangle perimeter. See <RectanglePerimeter> for a
+     * description of the parameters.
+     */
+    public static $TrianglePerimeter;
+}
---
+++ src/View/mxStyleRegistry.php
@@ -0,0 +1,65 @@
+<?php
+namespace Mxgraph\View;
+
+use Mxgraph\Util\mxConstants;
+
+/**
+ * Copyright (c) 2006-2013, Gaudenz Alder
+ */
+class mxStyleRegistry
+{
+
+	/**
+	 * Class: mxStyleRegistry
+	 *
+	 * Singleton class that acts as a global converter from string to object values
+	 * in a style. This is currently only used to perimeters and edge styles.
+	 *
+	 * 
+	 * Variable: values
+	 *
+	 * Maps from strings to objects.
+	 */
+	public static $values = array();
+
+	/**
+	 * Function: putValue
+	 *
+	 * Puts the given object into the registry under the given name.
+	 */
+	static function putValue($name, $value)
+	{
+		mxStyleRegistry::$values[$name] = $value;
+	}
+
+	/**
+	 * Function: getValue
+	 *
+	 * Returns the value associated with the given name.
+	 */
+	static function getValue($name)
+	{
+		return (isset(mxStyleRegistry::$values[$name])) ? mxStyleRegistry::$values[$name] : null;
+	}
+
+	/**
+	 * Function: getName
+	 * 
+	 * Returns the name for the given value.
+	 */
+	static function getName($value)
+	{
+		foreach (mxStyleRegistry::$values as $key => $val)
+		{
+			if ($value === $val)
+			{
+				return $key;
+			}
+		}
+		
+		return null;
+	}
+
+}
+
+?>
